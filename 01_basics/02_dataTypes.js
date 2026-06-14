68e73576e27064517e8a7e1632f6e95f9b6b7d66
Hammad Raza <mdhammadraza06@gmail.com>
2026-06-14 15:21:22 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as never version
+
+// alert ( 3+5) we are using node.js , not browser
+// so we dont use this alert 
+
+let name = " fiza"
+let age = 18 
+let isLoggedIn = false
+let state ;
+
+// data types 
+// number =>  2 to power 53
+//bigint  (very large number like in stock market etc )
+//string => "" ( preferred use to double quote can use single quote also)
+//boolean => true/false 
+//null => standalone value
+//undefined => when you not define the value of a string 
+//symbol => uniqueness 
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
+//null is object after checking typeof we get null is a object but not in 
+//undefined because undefined is a special type so null typeof  object is language error 
